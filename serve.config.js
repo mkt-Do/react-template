9e6f30bc1d4ff80efa61af56f07ce540f1ded3b6
mkt-Do <mkt.koro.mikuru@gmail.com>
2018-08-16 18:18:36 +0900

    correspond hot reload

--- serve.config.js
+++ serve.config.js
@@ -1,8 +1,23 @@
+const path = require('path');
+const chokidar = require('chokidar');
 const serve = require('webpack-serve');
 const config = require('./webpack.config.js');
 
+const add = (app, middleware, options) => {
+  middleware.webpack().then(result => {
+    const server = result.hotClient.server;
+    const watchPath = path.resolve(__dirname, 'public', '*');
+    const options = { ignoreInitial: true };
+    watcher = chokidar.watch(watchPath, options);
+    watcher.on('change', () => {
+      server.broadcast('{ "type": "window-reload", "data": {} }');
+    });
+  });
+};
+
 const argv =  {};
 const options = {
+  add,
   config,
   content: 'public/',
   host: '0.0.0.0',
